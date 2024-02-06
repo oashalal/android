b9328145f144657f07efcc98263114866aedc385
Your Name <you@example.com>
2024-02-06 14:59:47 +0300

    init

---
+++ app/src/main/java/com/oashalal/app/MainActivity.java
@@ -0,0 +1,17 @@
+package com.oashalal.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.text_view);
+        textView.setText("Hello, World!");
+    }
+}
