80e24b16b0b2287c8cd8fe672fa235953e951c00
Your Name <you@example.com>
2024-02-09 17:58:08 +0300

    move

--- core/src/com/oashalal/game/MyGame.java
+++ core/src/com/oashalal/game/MyGame.java
@@ -3,23 +3,44 @@ package com.oashalal.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Vector3;
 
 public class MyGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	int x = 300;
+	int y = 200;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 480);
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(1, 1, 1, 1);
+		
+		camera.update();
+        // tell the SpriteBatch to render in the
+        // coordinate system specified by the camera.
+        batch.setProjectionMatrix(camera.combined);
+        
+        if(Gdx.input.isTouched()) {
+            Vector3 touchPos = new Vector3();
+            touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+            camera.unproject(touchPos);
+            x = touchPos.x;
+            y = touchPos.y;
+        }
+
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, x, y);
 		batch.end();
 	}
 	
