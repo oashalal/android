24a9e9c55df052cd2bc96314528a1ab9550704fd
Your Name <you@example.com>
2024-02-09 07:38:58 +0300

    gdx

--- desktop/scripts/desktopDeployment/Mygame.bat
+++
@@ -1,2 +0,0 @@
-"jre/bin/java" -XX:MaxRAMPercentage=60 -classpath "lib/*" com.oashalal.game.desktopmodule.DesktopLauncher
-exit 0
--- desktop/scripts/desktopDeployment/Mygame.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-jre/bin/java -XX:MaxRAMPercentage=60 -classpath "lib/*" com.oashalal.game.desktopmodule.DesktopLauncher
-exit 0
