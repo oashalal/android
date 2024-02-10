c0ee98b09f2b957b46caffd42993db6b55d56d46
Your Name <you@example.com>
2024-02-10 11:19:08 +0300

    no

--- core/src/com/oashalal/game/MyGame.java
+++ core/src/com/oashalal/game/MyGame.java
@@ -25,13 +25,13 @@ public class MyGame extends ApplicationAdapter {
 	
 	@Override
 	public void create () {
-	    try{
+	    
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 		Skin mySkin = new Skin(Gdx.files.internal("skin/glassy-ui.json"));
-		camera = new OrthographicCamera();
-        camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        stage = new Stage(new ScreenViewport(camera));
+		//camera = new OrthographicCamera();
+        //camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        stage = new Stage(new ScreenViewport());
 		Gdx.input.setInputProcessor(stage);
 		
 		Button button = new TextButton("Вернуть",mySkin,"small");
@@ -51,28 +51,25 @@ public class MyGame extends ApplicationAdapter {
 			}
 		});
 		stage.addActor(button);
-	    } catch(Exception e){
-	        
-	    }
 	}
 
 	@Override
 	public void render () {
-	    try{
+	    
 		ScreenUtils.clear(1, 1, 1, 1);
 		
-		camera.update();
+		//camera.update();
         // tell the SpriteBatch to render in the
         // coordinate system specified by the camera.
-        batch.setProjectionMatrix(camera.combined);
+        //batch.setProjectionMatrix(camera.combined);
         
-        if(Gdx.input.isTouched()) {
+        /*if(Gdx.input.isTouched()) {
             Vector3 touchPos = new Vector3();
             touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
             //camera.unproject(touchPos);
             x = touchPos.x;
             y = touchPos.y;
-        }
+        }*/
 
 		batch.begin();
 		batch.draw(img, x, y);
@@ -80,9 +77,6 @@ public class MyGame extends ApplicationAdapter {
 		
 		stage.act(Gdx.graphics.getDeltaTime());
 		stage.draw();
-	    } catch(Exception e) {
-	        
-	    }
 	}
 	
 	@Override
