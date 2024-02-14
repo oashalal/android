401d62be2a4af2beebba2b45b3976ee06d8aba5c
Your Name <you@example.com>
2024-02-14 19:25:10 +0300

    auto

---
+++ app/src/main/java/com/oashalal/app/MainActivity.java
@@ -0,0 +1,30 @@
+package com.oashalal.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridLayout;
+
+public class MainActivity extends AppCompatActivity {
+    
+    GridLayout grid;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        grid = findViewById(R.id.grid);
+        
+        for(int i = 0; i<8; i++){
+            for(int j = 0; j<8; j++){
+                Button button = new Button(this);
+                button.setText(Integer.toString(i*8+j));
+                grid.addView(button);
+            }
+        }
+    }
+}
